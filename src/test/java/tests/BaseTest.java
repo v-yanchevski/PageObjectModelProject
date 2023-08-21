f320962e20056ed126fe9afcf7fb4af2d696d7a5
Vasil Yanchevski <vianchevski@gmail.com>
2023-08-21 07:55:05 +0300

    selenium updated to 4.11.0; browser changed to Chrome for testing

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,8 +11,13 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("PAHT_TO_chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "PAHT_TO_chromedriver.exe");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+
         driver.get("https://www.saucedemo.com/");
     }
 
